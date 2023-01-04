5298e5d36bfd938a61b1eabeeebd50e1fc5c400a
Afzal Hussain <azzu01122@gmail.com>
2023-01-04 21:29:26 +0530

    changes database from sqlite3 to mysql. but there is no data in database so json file datadump is also created for loading into database.

--- Ecart/settings.py
+++ Ecart/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'Ecart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_ecart',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
